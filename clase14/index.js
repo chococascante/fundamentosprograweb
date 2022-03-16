d030fd8c0d746760759260f526f075b48cf44863
Luis Cascante <luisalonzocascante@gmail.com>
2022-03-15 20:19:31 -0600

    clase14

--- clase14/index.js
+++ clase14/index.js
@@ -1,19 +1,29 @@
 window.onload = async function () {
     const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts');
     const datos = await respuesta.json();
+    
+    const respuestaUsuarios = await fetch('https://jsonplaceholder.typicode.com/users');
+    const usuarios = await respuestaUsuarios.json();
 
     const respuestaComentarios = await fetch('https://jsonplaceholder.typicode.com/comments');
     const datosComentarios = await respuestaComentarios.json();
+
+
     datos.forEach(function (publicacion) {
         const comentarios = datosComentarios.filter(function (comentario) {
             return publicacion.id === comentario.postId;
         });
-        crearPublicacion(publicacion, comentarios);
+
+        const usuario = usuarios.find(function (usuario) {
+            return publicacion.userId === usuario.id;
+        });
+
+        crearPublicacion(publicacion, usuario, comentarios);
     });
 
 }
 
-function crearPublicacion(publicacion, comentarios = []) {
+function crearPublicacion(publicacion = {}, usuario = {}, comentarios = []) {
     const contenedor = document.querySelector('#contenedor');
 
     const publicacionNueva = document.createElement('li');
@@ -21,24 +31,36 @@ function crearPublicacion(publicacion, comentarios = []) {
     const titulo = document.createElement('h3');
     titulo.innerHTML = formatearTitulo(publicacion.title);
 
+    const autorPublicacion = document.createElement('caption');
+    autorPublicacion.innerHTML = usuario.email;
+
     const cuerpo = document.createElement('p');
     cuerpo.innerHTML = publicacion.body;
 
     publicacionNueva.appendChild(titulo);
+    publicacionNueva.appendChild(autorPublicacion);
     publicacionNueva.appendChild(cuerpo);
+
 
     const listaComentarios = document.createElement('ol');
 
     comentarios.forEach(function (comentario) {
-        const item = document.createElement('li');
+        const elemento = document.createElement('li');
+
         const nombre = document.createElement('h4');
-        nombre = formatearTitulo(comentario.name);
-        item.appendChild(nombre);
+        nombre.innerHTML = formatearTitulo(comentario.name);
+        elemento.appendChild(nombre);
 
+        const autor = document.createElement('h5');
+        autor.innerHTML = comentario.email;
+        elemento.appendChild(autor);
 
+        const cuerpoComentario = document.createElement('p');
+        cuerpoComentario.innerHTML = comentario.body;
+        elemento.appendChild(cuerpoComentario);
 
-        listaComentarios.appendChild(item);
-        
+        listaComentarios.appendChild(elemento);
+
     });
     publicacionNueva.appendChild(listaComentarios);
     contenedor.appendChild(publicacionNueva);
