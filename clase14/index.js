14ea57c34fa2f4995d6add7704d4999b5c98cf58
Luis Cascante <luisalonzocascante@gmail.com>
2022-03-18 10:40:23 -0600

    clase 15

--- clase14/index.js
+++ clase14/index.js
@@ -1,71 +1,71 @@
 window.onload = async function () {
-    const respuesta = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const datos = await respuesta.json();
-    
-    const respuestaUsuarios = await fetch('https://jsonplaceholder.typicode.com/users');
-    const usuarios = await respuestaUsuarios.json();
+  const respuesta = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const datos = await respuesta.json();
 
-    const respuestaComentarios = await fetch('https://jsonplaceholder.typicode.com/comments');
-    const datosComentarios = await respuestaComentarios.json();
+  const respuestaUsuarios = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  const usuarios = await respuestaUsuarios.json();
 
+  const respuestaComentarios = await fetch(
+    "https://jsonplaceholder.typicode.com/comments"
+  );
+  const datosComentarios = await respuestaComentarios.json();
 
-    datos.forEach(function (publicacion) {
-        const comentarios = datosComentarios.filter(function (comentario) {
-            return publicacion.id === comentario.postId;
-        });
-
-        const usuario = usuarios.find(function (usuario) {
-            return publicacion.userId === usuario.id;
-        });
-
-        crearPublicacion(publicacion, usuario, comentarios);
+  datos.forEach(function (publicacion) {
+    const comentarios = datosComentarios.filter(function (comentario) {
+      return publicacion.id === comentario.postId;
     });
 
-}
+    const usuario = usuarios.find(function (usuario) {
+      return publicacion.userId === usuario.id;
+    });
+
+    crearPublicacion(publicacion, usuario, comentarios);
+  });
+};
 
 function crearPublicacion(publicacion = {}, usuario = {}, comentarios = []) {
-    const contenedor = document.querySelector('#contenedor');
+  const contenedor = document.querySelector("#contenedor");
 
-    const publicacionNueva = document.createElement('li');
+  const publicacionNueva = document.createElement("li");
 
-    const titulo = document.createElement('h3');
-    titulo.innerHTML = formatearTitulo(publicacion.title);
+  const titulo = document.createElement("h3");
+  titulo.innerHTML = formatearTitulo(publicacion.title);
 
-    const autorPublicacion = document.createElement('caption');
-    autorPublicacion.innerHTML = usuario.email;
+  const autorPublicacion = document.createElement("caption");
+  autorPublicacion.innerHTML = usuario.email;
 
-    const cuerpo = document.createElement('p');
-    cuerpo.innerHTML = publicacion.body;
+  const cuerpo = document.createElement("p");
+  cuerpo.innerHTML = publicacion.body;
 
-    publicacionNueva.appendChild(titulo);
-    publicacionNueva.appendChild(autorPublicacion);
-    publicacionNueva.appendChild(cuerpo);
+  publicacionNueva.appendChild(titulo);
+  publicacionNueva.appendChild(autorPublicacion);
+  publicacionNueva.appendChild(cuerpo);
 
+  const listaComentarios = document.createElement("ol");
 
-    const listaComentarios = document.createElement('ol');
+  comentarios.forEach(function (comentario) {
+    const elemento = document.createElement("li");
 
-    comentarios.forEach(function (comentario) {
-        const elemento = document.createElement('li');
+    const nombre = document.createElement("h4");
+    nombre.innerHTML = formatearTitulo(comentario.name);
+    elemento.appendChild(nombre);
 
-        const nombre = document.createElement('h4');
-        nombre.innerHTML = formatearTitulo(comentario.name);
-        elemento.appendChild(nombre);
+    const autor = document.createElement("h5");
+    autor.innerHTML = comentario.email;
+    elemento.appendChild(autor);
 
-        const autor = document.createElement('h5');
-        autor.innerHTML = comentario.email;
-        elemento.appendChild(autor);
+    const cuerpoComentario = document.createElement("p");
+    cuerpoComentario.innerHTML = comentario.body;
+    elemento.appendChild(cuerpoComentario);
 
-        const cuerpoComentario = document.createElement('p');
-        cuerpoComentario.innerHTML = comentario.body;
-        elemento.appendChild(cuerpoComentario);
-
-        listaComentarios.appendChild(elemento);
-
-    });
-    publicacionNueva.appendChild(listaComentarios);
-    contenedor.appendChild(publicacionNueva);
+    listaComentarios.appendChild(elemento);
+  });
+  publicacionNueva.appendChild(listaComentarios);
+  contenedor.appendChild(publicacionNueva);
 }
 
-function formatearTitulo(titulo = '') {
-    return `${titulo[0].toUpperCase()}${titulo.slice(1)}`;
+function formatearTitulo(titulo = "") {
+  return `${titulo[0].toUpperCase()}${titulo.slice(1)}`;
 }
---
+++ clase15/TareaServicios.md
@@ -0,0 +1,7 @@
+# Tarea Servicios
+
+1. Crear una función que se ejecute cuando termina de cargar el html (window.onload).
+2. Hacer que esa función llame al servicio de todos.
+3. Hacer que esa función llame al servicio de usuarios.
+4. Crear el html para que cada una de las tareas por hacer (todo) se muestre en la página.
+5. Incluir dentro de ese HTML el nombre del usuario que creó la tarea (tienen que buscarla en el arreglo de usuarios.)
